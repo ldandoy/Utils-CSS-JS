e044bd175c9ca6ad183183e03db1a6ad568aef4d
ldandoy <ldandoy@overconsulting.net>
2021-10-08 17:01:49 +0200

    MAJ

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,8 +3,8 @@ let icon = document.querySelector('.navbar-menu-icon');
 
 const showNavbar = () => {
     navbar.classList.toggle('responsive');
-}
+};
 
 icon.addEventListener("click", (event) => {
     showNavbar();
-})
+});
