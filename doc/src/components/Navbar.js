a8ba50b88f695d9d73f4c4c37700e02deea42183
ldandoy <ldandoy@overconsulting.net>
2021-11-11 11:39:30 +0100

    Correction CSS navbar

--- doc/src/components/Navbar.js
+++ doc/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    return <nav className="navbar navbar-v navbar-bordered-b bg-blue-800 navbar-dark sticky-top">
+    return <nav className="navbar bg-blue-800 navbar-dark sticky-top">
         <div className="navbar-title">
             <div>
                 <Link className="navbar-link" to="/">
@@ -13,13 +13,15 @@ const Navbar = () => {
             <i className="navbar-menu-icon">&#9776;</i>
         </div>
         <div className="navbar-content-menu">
-            <ul className="navbar-menu-left">
+            <ul className="navbar-menu navbar-menu-left">
                 <li className="navbar-item">
                     <Link className="navbar-link" to="/">Doc CSS</Link>
+                </li>
+                <li className="navbar-item">
                     <Link className="navbar-link" to="/">Doc JS</Link>
                 </li>
             </ul>
-            <ul className="navbar-menu-right">
+            <ul className="navbar-menu navbar-menu-right">
                 <li className="navbar-item">
                     <a className="navbar-link" rel="noreferrer" href="https://github.com/ldandoy/Utils-CSS-JS" target="_blank">Repos GitHub</a>
                 </li>
