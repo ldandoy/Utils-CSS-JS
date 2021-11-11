7ecb7fd773c0792ff2826ff90c4c732770f27420
ldandoy <ldandoy@overconsulting.net>
2021-11-11 11:05:15 +0100

    Correction JS

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let open = document.querySelector('.modal-open');
-    open.addEventListener("click", (event) => {
-        event.preventDefault();
-        let modal = document.querySelector('#' + open.getAttribute('target-modal'));
-        modal.classList.add("active");
-    });
-    
-    let close = document.querySelector('.modal-close');
-    close.addEventListener("click", (event) => {
-        event.preventDefault();
-        close.parentElement.parentElement.parentElement.classList.remove('active');
-    });
+    if (open) {
+        open.addEventListener("click", (event) => {
+            event.preventDefault();
+            let modal = document.querySelector('#' + open.getAttribute('target-modal'));
+            modal.classList.add("active");
+        });
+        
+        let close = document.querySelector('.modal-close');
+        close.addEventListener("click", (event) => {
+            event.preventDefault();
+            close.parentElement.parentElement.parentElement.classList.remove('active');
+        });
+    }
 });
