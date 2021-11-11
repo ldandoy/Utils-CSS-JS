36fb6af8bd6714d05df2085094c16c39dda2d3aa
ldandoy <ldandoy@overconsulting.net>
2021-11-11 12:52:45 +0100

    Add Js effect

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -20,8 +20,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
             toggle.classList.toggle('active');
             console.log(toggle.getAttribute('text-active'));
             if (toggle.classList.contains('active')) {
+                console.log('active');
                 toggle.innerHTML = toggle.getAttribute('text-active');
             } else {
+                console.log('non active');
                 toggle.innerHTML = toggle.getAttribute('text-nonactive');
             }
 
