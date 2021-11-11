f4eb6287ab6a67c1c3e337f538e312e201b7f57e
ldandoy <ldandoy@overconsulting.net>
2021-11-11 11:03:42 +0100

    MAJ

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,24 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Get all elements with class "toggle"
     let toggles = document.getElementsByClassName('toggle');
 
-    // Creating toggles
     for (let toggle of toggles) {
         let targets = document.querySelectorAll("." + toggle.getAttribute('target-show'));
         
-        // Hide the target
         for (let target of targets) {
             target.classList.add('toggle-target');
         }
 
         targets = document.querySelectorAll("." + toggle.getAttribute('target-hide'));
         
-        // Hide the target
         for (let target of targets) {
             target.classList.add('toggle-target');
         }
         
-        // Add event to display target on click
         toggle.addEventListener('click', (event) => {
             event.preventDefault();
             let targetsShow = document.querySelectorAll("." + toggle.getAttribute('target-show'));
