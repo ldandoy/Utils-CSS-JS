8c7c8ca744ecfbae129eae3c7d77ce1fd33fbec4
R3tt4h <kevin953@outlook.com>
2022-02-02 12:18:29 +0100

    feat: add copyToClipboard + recoilShowIn

--- doc/src/components/ThemeMode.js
+++ doc/src/components/ThemeMode.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { atom, useRecoilState } from "recoil";
 import { Icon } from '@iconify/react';
 import './ThemeMode.css'
@@ -10,11 +10,17 @@ export const theme = atom({
 
 const ThemeMode = () => {
   const [themeMode, setThemeMode] = useRecoilState(theme);
+  const [iconColorSun, setIconColorSun] = useState("gold")
+  const [iconColorMoon, setIconColorMoon] = useState("808080")
+
 
   useEffect(() => {
     const bodyElement = document.querySelector("body");
 
     themeMode ? bodyElement.classList.add("dark") : bodyElement.classList.remove("dark");
+    themeMode ? setIconColorSun("808080") : setIconColorSun("gold");
+    themeMode ? setIconColorMoon("gold") : setIconColorMoon("808080");
+
   }, [themeMode]);
 
   return (
@@ -27,8 +33,8 @@ const ThemeMode = () => {
             onChange={() => setThemeMode(!themeMode)}
           />
         <label htmlFor="switch">
-            <Icon className="sun" icon="eva:sun-fill" color="gold" />
-            <Icon className="moon" icon="eva:moon-fill" color="fff" />
+            <Icon className="sun" icon="eva:sun-fill" color= {iconColorSun}/>
+            <Icon className="moon" icon="eva:moon-fill" color={iconColorMoon}/>
         </label>
       </div>
     </div>
